a05cb07002c18a9025ee9c6044ddfc5bc76377d8
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-10-12 19:37:08 -0300

    Se agregaron Snickers y Super8 como productos.

---
+++ Snickers.java
@@ -0,0 +1,11 @@
+public class Snickers extends Dulces{
+    private int nserie;
+    public Snickers(int nserie){
+        super(nserie);
+    }
+    @Override
+    public String sabor() {
+        return "Sabor a: snicker que rico";
+    }
+}
+
---
+++ Super8.java
@@ -0,0 +1,9 @@
+public class Super8 extends Dulces{
+    public Super8(int nserie){
+        super(nserie);
+    }
+    @Override
+    public String sabor() {
+        return "Sabor a: super8 que rico";
+    }
+}
