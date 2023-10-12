f40a6a50d99b0145694ea6659f93d7a13147f60c
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-10-12 20:06:07 -0300

    Mejoramiento en los depositos del expendedor.

--- Expendedor.java
+++ Expendedor.java
@@ -1,27 +1,30 @@
 class Expendedor {
     public static final int COCA=1;
     public static final int SPRITE=2;
-    private Deposito coca;
-    private Deposito sprite;
-    public DepositoM monVu;
-    public Deposito Snickers;
-    public Deposito Super8;
+    private Deposito<Bebida> coca;
+    private Deposito<Bebida> sprite;
+    private Deposito<Bebida> fanta;
+    public DepositoM<Moneda> monVu;
+    public Deposito<Dulces> Snickers;
+    public Deposito<Dulces> Super8;
     private int precioBebidas;
     private int precioDulces;
 
     public Expendedor(int numBebibas,int precioBebidas,int numDulces,int precioDulces) {
         this.precioBebidas = precioBebidas;
-        this.coca = new Deposito();
-        this.sprite = new Deposito();
-        this.monVu = new DepositoM();
-        this.Snickers= new Deposito();
-        this.Super8 = new Deposito();
+        this.coca = new Deposito<>();
+        this.sprite = new Deposito<>();
+        this.monVu = new DepositoM<>();
+        this.Snickers= new Deposito<>();
+        this.Super8 = new Deposito<>();
         for (int i = 0; i < numBebibas; i++) {
             Bebida b1 = new CocaCola(i);
             Bebida b2 = new Sprite(i);
+            Bebida b3 = new Fanta(i);
 
             coca.addProducto(b1);
             sprite.addProducto(b2);
+            fanta.addProducto(b3);
         }
         for(int i=0; i<numDulces;i++){
             Dulces d1 = new Snickers(i);
@@ -31,7 +34,7 @@ class Expendedor {
         }
     }
     public Bebida comprarBebida(Moneda moneda,int selector){
-        Bebida b;
+        Producto b;
         if(moneda==null) {
             return null;
         }
