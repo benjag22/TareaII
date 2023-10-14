97c99da01452846e60b2b834983d5d48be008142
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-10-14 10:42:46 -0300

    Try-Catch en main para los errores.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoHayProductoException, PagoIncorrectoException,PagoInsuficienteException {
         Expendedor exp = new Expendedor(2);
         Moneda m;
         Comprador c;
@@ -21,39 +21,102 @@ public class Main {
         c = new Comprador(m,5,exp);
         System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
 
-        //deberia ser PagoIncorrectoException(aun no lo es)//
-        m=null;
-        c = new Comprador(m,1,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m=null;
-        c = new Comprador(m,2,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m=null;
-        c = new Comprador(m,3,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m=null;
-        c = new Comprador(m,4,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m=null;
-        c = new Comprador(m,5,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+        //PagoIncorrectoException//
+        try {
+            m = null;
+            c = new Comprador(m, 1, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
+        try {
+            m = null;
+            c = new Comprador(m, 2, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
+        try {
+            m = null;
+            c = new Comprador(m, 3, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
+        try {
+            m = null;
+            c = new Comprador(m, 3, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
+        try {
+            m = null;
+            c = new Comprador(m, 4, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
+        try {
+            m = null;
+            c = new Comprador(m, 5, exp);
+        }
+        catch (PagoIncorrectoException e){
+            System.out.println("Error: Pago incorrecto");
+            System.out.println(null+", "+0);
+        }
 
-        //*deberia ser PagoInsuficienteException(aun no lo es)//
-        m = new Moneda100();
-        c = new Comprador(m,1,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,2,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,3,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,4,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,5,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+        //PagoInsuficienteException//
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 1, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch (PagoInsuficienteException e) {
+            System.out.println("Error: Pago insuficiente");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 2, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch (PagoInsuficienteException e) {
+            System.out.println("Error: Pago insuficiente");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 3, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch (PagoInsuficienteException e) {
+            System.out.println("Error: Pago insuficiente");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 4, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch (PagoInsuficienteException e) {
+            System.out.println("Error: Pago insuficiente");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda100();
+            c = new Comprador(m, 5, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch (PagoInsuficienteException e) {
+            System.out.println("Error: Pago insuficiente");
+            System.out.println(null+", "+m.getValor());
+        }
 
         //compra exitosa para cada producto//
         m = new Moneda1000();
@@ -72,23 +135,51 @@ public class Main {
         c = new Comprador(m,5,exp);
         System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
 
-        //deberia ser NoHayProductoException(aun no lo es)//
-        m = new Moneda1500();
-        c = new Comprador(m,1,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda1500();
-        c = new Comprador(m,2,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda1500();
-        c = new Comprador(m,3,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda1500();
-        c = new Comprador(m,4,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-        m = new Moneda1500();
-        c = new Comprador(m,5,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-
+        //NoHayProductoException//
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, 1, exp);
+        }
+        catch(NoHayProductoException e){
+            System.out.println("No hay producto del tipo solicitado");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, 2, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch(NoHayProductoException e){
+            System.out.println("No hay producto del tipo solicitado");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, 3, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch(NoHayProductoException e){
+            System.out.println("No hay producto del tipo solicitado");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, 4, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch(NoHayProductoException e){
+            System.out.println("No hay producto del tipo solicitado");
+            System.out.println(null+", "+m.getValor());
+        }
+        try {
+            m = new Moneda1500();
+            c = new Comprador(m, 5, exp);
+            System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        }
+        catch(NoHayProductoException e){
+            System.out.println("No hay producto del tipo solicitado");
+            System.out.println(null+", "+m.getValor());
+        }
 
     }
 }
